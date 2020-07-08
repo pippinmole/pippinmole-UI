b88f89517d677faaccddb91aa91c7f73152422e7
pippin_mole <40035529+pippinmole@users.noreply.github.com>
2020-07-08 02:09:40 +0100

    BillboardWorldSpaceUI

---
+++ Runtime/Components/BillboardWorldSpaceUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Decay.Utilities;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+[ExecuteAlways]
+public class BillboardWorldSpaceUI : MonoBehaviour {
+    private void OnEnable() {
+        //Camera.onPreRender += this.PreRender;
+        RenderPipelineManager.beginCameraRendering += this.PreRender;
+    }
+
+    private void OnDisable() {
+        //Camera.onPreRender -= this.PreRender;
+        RenderPipelineManager.beginCameraRendering -= this.PreRender;
+    }
+
+    private void PreRender(ScriptableRenderContext ctx, Camera cam) {
+        this.transform.forward = cam.transform.forward;
+    }
+}
